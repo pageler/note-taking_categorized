c5778e9965e0569639461c5bdde886f2fa444635
John Paul Pageler Jr <harakido1@yahoo.com>
2023-11-19 11:30:19 -0500

    NoteLayout for NoteDetails & EditNote

--- src/components/NoteLayout.tsx
+++ src/components/NoteLayout.tsx
@@ -1,9 +1,30 @@
-type NoteLayoutProps = {};
+import {
+    Navigate,
+    Outlet,
+    useOutletContext,
+    useParams,
+} from "react-router-dom";
+import { Note } from "../App";
 
-export const NoteLayout = (props: NoteLayoutProps) => {
+type NoteLayoutProps = {
+    notes: Note[];
+};
+
+export const NoteLayout = ({ notes }: NoteLayoutProps) => {
+    const { id } = useParams();
+    const note = notes.find((note) => note.id === id);
+
+    if (note == null) {
+        return <Navigate to="/" replace />;
+    }
+
     return (
         <>
-            <h1>NoteLayout</h1>
+            <Outlet context={note} />
         </>
     );
 };
+
+export function useNote() {
+    return useOutletContext<Note>();
+}
